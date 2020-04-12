c7d95625d085cfad2dbe1b5e5464b2495e3d6c02
zernst3 <60371221+zernst3@users.noreply.github.com>
2020-04-12 14:12:02 -0400

    Add files via upload

---
+++ Sidebar.js
@@ -0,0 +1,8 @@
+$(function() {
+    // Sidebar toggle behavior
+    $('#sidebarCollapse').on('click', function() {
+      $('#sidebar, #content, .toggle-button').toggleClass('active');
+    });
+  });
+
+  
